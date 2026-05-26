a6500ce3eed2bfc272860641c250351dcb11a95a
Hiren Siyodia | Front End Developer <siyodiahiren7@gmail.com>
2026-05-26 17:35:20 +0530

    Update footer

--- Beginner/musicnextjs/components/ui/Footer.tsx
+++ Beginner/musicnextjs/components/ui/Footer.tsx
@@ -80,7 +80,7 @@ function Footer() {
           <p>Phone: (123) 456-7890</p>
         </div>
         </div>
-        <p className="text-center text-xs pt-8">© 2026 Music School. All rights reserved.</p>
+        <p className="text-center text-xs pt-8">© 2026 Hiren Siyodia. All rights reserved.</p>
     </footer>
   )
 }
